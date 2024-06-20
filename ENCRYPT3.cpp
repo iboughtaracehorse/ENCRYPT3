e35e3a4bef5b8bf632fc459c6431fe3307b458d0
Dariia Dushynska <ddushynska@kse.org.ua>
2024-06-20 19:28:31 +0300

    user input fixed. decrypt from buffer bug

--- ENCRYPT3.cpp
+++ ENCRYPT3.cpp
@@ -8,54 +8,80 @@ int main() {
     char* buffer = new char[256];
     char* mode = new char[20];
     int key = 1;
+    bool end = false;
 
-    std::cout << "Enter a mode: ";
-    std::cin >> mode;
+    while (!end) {
+        std::cout << "Enter a mode(encrypt/decrypt/exit): ";
+        std::cin >> mode;
 
-    std::cout << "Enter a text: ";
-    std::cin.ignore();
-    std::cin.getline(rawText, initialSize);
-
-    std::cout << "Enter a key: ";
-    std::cin >> key;
-
-   
-    if (strcmp(mode, "encrypt") == 0) {
-        char* encrypted = encrypt(rawText, key);
-        std::cout << "Encrypted: " << encrypted << std::endl;
-
-        strcpy_s(buffer,initialSize, encrypted);
-
-        delete[] encrypted;
-        encrypted = nullptr;
-    }
-    else if (strcmp(mode, "decrypt") == 0) {
-        int decryptMode;
-        char* decrypted = new char[256];
-        std::cout << "To decrypt text from buffer enter 1\n To decrypt new text enter 2: ";
-        std::cin >> decryptMode;
-
-        if (decryptMode == 1) {
-            char* decrypted = decrypt(buffer, key);
-            std::cout << "Decrypted: " << decrypted << std::endl;
-
-        }
-        else if (decryptMode == 2) {
-            char* decrypted = decrypt(rawText, key);
-            std::cout << "Decrypted: " << decrypted << std::endl;
+        if (strcmp(mode, "exit") == 0) {
+            return 0;
         }
 
-        
+        if (strcmp(mode, "encrypt") == 0) {
+            std::cout << "Enter a text: ";
+            std::cin.ignore();
+            std::cin.getline(rawText, initialSize);
 
-        delete[] decrypted;
-        decrypted = nullptr;
+            std::cout << "Enter a key: ";
+            std::cin >> key;
+
+            char* encrypted = encrypt(rawText, key);
+            std::cout << "Encrypted: " << encrypted << std::endl;
+
+            strcpy_s(buffer, initialSize, encrypted);
+
+            delete[] encrypted;
+            encrypted = nullptr;
+        }
+
+        else if (strcmp(mode, "decrypt") == 0) {
+            int decryptMode;
+            char* decrypted = new char[256];
+            std::cout << "To decrypt text from buffer enter 1\nTo decrypt new text enter 2: ";
+            std::cin >> decryptMode;
+
+            if (decryptMode == 1) {
+
+                if (buffer[0] == '\0') {
+                    std::cerr << "Buffer is empty! No text to decrypt." << std::endl;
+                }
+                else {
+                    char* decrypted = decrypt(buffer, key);
+                    std::cout << "Decrypted: " << decrypted << std::endl;
+
+                }
+              
+            }
+            else if (decryptMode == 2) {
+
+                std::cout << "Enter a text: ";
+                std::cin.ignore();
+                std::cin.getline(rawText, initialSize);
+
+                std::cout << "Enter a key: ";
+                std::cin >> key;
+
+                char* decrypted = decrypt(rawText, key);
+                std::cout << "Decrypted: " << decrypted << std::endl;
+            }
+
+
+
+            delete[] decrypted;
+            decrypted = nullptr;
+        }
+        else {
+            std::cerr << "Invalid mode!" << std::endl;
+        }
     }
-    else {
-        std::cerr << "Invalid mode!" << std::endl;
-    }
+
+  
 
     delete[] rawText;
     rawText = nullptr;
+    delete[] buffer;
+    buffer = nullptr;
     delete[] mode;
     mode = nullptr;
 
